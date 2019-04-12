b9bb8be3c6f5cdf3b862a7786364e5d2a82ba23b
weezer924 <weezer970@gmail.com>
2019-04-13 06:19:21 +0900

    add a sample model to keep data from kintone

---
+++ angular-http/src/app/models/sample.model.ts
@@ -0,0 +1,8 @@
+export class SampleModel {
+  constructor(
+    // These two is basic value in kintone tables
+    // Customize your own value for the model
+    public $revision?: string,
+    public id?: number,
+  ) {}
+}
